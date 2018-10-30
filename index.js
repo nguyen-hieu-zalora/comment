a80d7354b22104957ac188668ced9632736a83ca
nguyen.hieu <nguyen.hieu@zalora.com>
2018-10-30 17:50:02 +0800

    add service worker

--- index.js
+++ index.js
@@ -1,55 +1,72 @@
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  function getCommentText(comments, counter) {
-    return '"' + comments[counter][0] + '"'
-  }
-  function getCountryText(comments, counter) {
-    return comments[counter][1]
-  }
-  function setText(commentText, countryText, maxLength) {
-      var comment = document.getElementById('comment');
-      var country = document.getElementById('country');
-      comment.textContent = commentText
-      country.textContent = countryText
-      if (commentText.length > maxLength) {
-          comment.classList.add('long-text');
-      } else {
-          comment.classList.remove('long-text');
-      }
-  }
-  function getCountryTexts(comments, country) {
-      var countryTexts = [];
-      var hasFound = false;
-      var numberOfComments = comments.length;
-      for (var i = 0; i < numberOfComments; ++i) {
-          var textCountry = comments[i][1];
-          if (textCountry == country) {
-              hasFound = true;
-              countryTexts.push(comments[i]);
-          } else if (hasFound) {
-              // Assuming all countries comments stay together
-              break;
-          }
-      }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: './' }).then(function(reg) {
 
-      return countryTexts;
-  }
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
 
-  function getCountryParam() {
-      var countries = {
-        'sg': 'Singapore',
-        'ph': 'Philippines',
-        'my': 'Malaysia',
-        'hk': 'Hong Kong',
-        'tw': 'Taiwan',
-        'id': 'Indonesia'
-      };
-      var params = new URL(location.href).searchParams;
-      var raw_country = params.get('country');
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
 
-      return typeof(countries[raw_country]) == 'undefined' ? '' : countries[raw_country];
-  }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function getCommentText(comments, counter) {
+  return '"' + comments[counter][0] + '"'
+}
+function getCountryText(comments, counter) {
+  return comments[counter][1]
+}
+function setText(commentText, countryText, maxLength) {
+    var comment = document.getElementById('comment');
+    var country = document.getElementById('country');
+    comment.textContent = commentText
+    country.textContent = countryText
+    if (commentText.length > maxLength) {
+        comment.classList.add('long-text');
+    } else {
+        comment.classList.remove('long-text');
+    }
+}
+function getCountryTexts(comments, country) {
+    var countryTexts = [];
+    var hasFound = false;
+    var numberOfComments = comments.length;
+    for (var i = 0; i < numberOfComments; ++i) {
+        var textCountry = comments[i][1];
+        if (textCountry == country) {
+            hasFound = true;
+            countryTexts.push(comments[i]);
+        } else if (hasFound) {
+            // Assuming all countries comments stay together
+            break;
+        }
+    }
+
+    return countryTexts;
+}
+
+function getCountryParam() {
+    var countries = {
+      'sg': 'Singapore',
+      'ph': 'Philippines',
+      'my': 'Malaysia',
+      'hk': 'Hong Kong',
+      'tw': 'Taiwan',
+      'id': 'Indonesia'
+    };
+    var params = new URL(location.href).searchParams;
+    var raw_country = params.get('country');
+
+    return typeof(countries[raw_country]) == 'undefined' ? '' : countries[raw_country];
+}
 
 function displayText(comments) {
     var timeToWait = 10000;
---
+++ sw.js
@@ -0,0 +1,102 @@
+var cacheVersion = 0;
+var cacheVersionKey = 'v' + parseInt(cacheVersion + 1);
+var cacheUrls = {
+  '/index.html': 0,
+  '/index.css': 1,
+  '/index.js': 2,
+  '/index.json': 3,
+  'https://static-sg.zacdn.com/fonts/apercu/apercu_regular-webfont.woff2': 4,
+}
+
+var makeRequest = function(event) {
+  return fetch(event.request).then(function (response) {
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    let responseClone = response.clone();
+
+    if (response.status < 400) {
+      caches.open(cacheVersionKey).then(function (cache) {
+        cache.put(event.request, responseClone);
+      });
+    } else {
+        console.log('Not caching the response to', event.request.url);
+    }
+
+    return response;
+  }).catch(function (error) {
+    console.error('Error in fetch handler:', error);
+    throw error;
+  });
+}
+
+self.addEventListener('install', function(event) {
+  // Skip the 'waiting' lifecycle phase, to go directly from 'installed' to 'activated', even if
+  // there are still previous incarnations of this service worker registration active.
+  event.waitUntil(
+    caches.open(cacheVersionKey).then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/index.css',
+        '/index.js',
+        '/index.json',
+        'https://static-sg.zacdn.com/fonts/apercu/apercu_regular-webfont.woff2',
+      ]);
+    })
+  );
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      console.log('Found response in cache:', response);
+
+      makeRequest(event);
+
+      return response;
+    } else {
+      console.log(' No response for %s found in cache. About to fetch ' +
+          'from network...', event.request.url);
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+
+        var host = self.location.origin;
+        var path = event.request.url.replace(host, '');
+        var needCache = typeof(cacheUrls[path]) !== 'undefined';
+
+        if (needCache && response.status < 400) {
+          caches.open(cacheVersionKey).then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+        } else {
+            console.log('Not caching the response to', event.request.url);
+        }
+
+        return response;
+      }).catch(function (error) {
+        console.error('  Error in fetch handler:', error);
+        throw error;
+      });
+    }
+  }));
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheKeeplist = [cacheVersionKey];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
