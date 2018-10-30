e3b789dda4149ca7ddef2325f0f2d1819937dcd0
nguyen.hieu <nguyen.hieu@zalora.com>
2018-10-30 16:14:35 +0800

    move code to separate files

---
+++ index.js
@@ -0,0 +1,79 @@
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  function getCommentText(comments, counter) {
+    return '"' + comments[counter][0] + '"'
+  }
+  function getCountryText(comments, counter) {
+    return comments[counter][1]
+  }
+  function setText(commentText, countryText, maxLength) {
+      var comment = document.getElementById('comment');
+      var country = document.getElementById('country');
+      comment.textContent = commentText
+      country.textContent = countryText
+      if (commentText.length > maxLength) {
+          comment.classList.add('long-text');
+      } else {
+          comment.classList.remove('long-text');
+      }
+  }
+  function getCountryTexts(comments, country) {
+      var countryTexts = [];
+      var hasFound = false;
+      var numberOfComments = comments.length;
+      for (var i = 0; i < numberOfComments; ++i) {
+          var textCountry = comments[i][1];
+          if (textCountry == country) {
+              hasFound = true;
+              countryTexts.push(comments[i]);
+          } else if (hasFound) {
+              // Assuming all countries comments stay together
+              break;
+          }
+      }
+
+      return countryTexts;
+  }
+
+  function getCountryParam() {
+      var countries = {
+        'sg': 'Singapore',
+        'ph': 'Philippines',
+        'my': 'Malaysia',
+        'hk': 'Hong Kong',
+        'tw': 'Taiwan',
+        'id': 'Indonesia'
+      };
+      var params = new URL(location.href).searchParams;
+      var raw_country = params.get('country');
+
+      return typeof(countries[raw_country]) == 'undefined' ? '' : countries[raw_country];
+  }
+
+function displayText(comments) {
+    var timeToWait = 10000;
+    var maxLength = 200; // should allow for 5 line with 40 characters per line
+    var comments = comments || [];
+    var countryParam = getCountryParam();
+
+    if (countryParam != '') {
+        comments = getCountryTexts(comments, countryParam);
+    }
+
+    var comment = document.getElementById('comment');
+    var country = document.getElementById('country');
+    var numberOfComments = comments.length;
+
+    if (numberOfComments) {
+      var counter = getRandomInt(numberOfComments);
+      setText(getCommentText(comments, counter), getCountryText(comments, counter), maxLength)
+      setInterval(function() {
+          counter = (counter + 1) % comments.length;
+          setText(getCommentText(comments, counter), getCountryText(comments, counter))
+      }, timeToWait);
+    }
+}
+
+var data = document.getElementById('data');
+data.addEventListener('onload', displayText(comments));
